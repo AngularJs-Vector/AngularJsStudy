ffbd6a9de33942f0c7dc291fde2cb74ec11697d2
VectorHuang <642378415@qq.com>
2016-03-25 10:50:07 +0800

    引入RequireJs 来做模块化布局

--- js/appAdmin/admin_main.js
+++ js/appAdmin/admin_main.js
@@ -2,3 +2,16 @@
  * author: vector.huang
  * date：2016/3/25 9:51
  */
+require.config({
+    baseUrl:"../js/appAdmin",
+    paths : {
+        "angular" : "../../framework/angular/angular",
+        "network" :"../common/network"
+    }
+})
+
+require(["angular","admin_app","admin_services","network"],function(){
+    $(function(){
+        alert("load finished");
+    })
+})
---
+++ js/appTest/require.a.js
@@ -0,0 +1,8 @@
+/**
+ * author: vector.huang
+ * date：2016/3/25 10:03
+ */
+function fun1(){
+    alert("Hello World!!")
+}
+fun1()
